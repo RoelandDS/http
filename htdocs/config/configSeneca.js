c7316bd6beca0a10bccc7380792044b137d2ddc9
Daan Flore <daan.flore.u8852@student.hogent.be>
2016-02-10 11:23:26 +0100

    adding route

--- htdocs/config/configSeneca.js
+++ htdocs/config/configSeneca.js
@@ -1,4 +1,5 @@
 module.exports = function(seneca) {
   seneca
-    .client({port: 3501, pin: {role: 'clients', cmd:'*'}, map:{getClients: {GET:true}}});
+    //.client({port: 3501, pin: {role: 'clients', cmd:'*'}, map:{getClients: {GET:true}}})
+    .client({port: 3500, pin: {role: 'weather', cmd:'*'}, map:{getWeather: {GET:true}}});
 };
--- htdocs/lib/routes.js
+++ htdocs/lib/routes.js
@@ -5,15 +5,31 @@ var	configSeneca = require('../config/configSeneca.js');
 configSeneca(seneca);
 
 module.exports = function(app){
-	app.get('/', function(req, res){
+	/*app.get('/test', function(req, res){
 		seneca.act({ role: 'clients', cmd: 'getClients'}, function(err, result){
 			if (err) console.log(err);
-			console.log(result);
+			/*console.log(result);
 			res.status(200).render(__dirname + '/templates/index.jade', {
-				message: result.message
+				message: /*result.message
 			});
 		});
 
-		
+
+
+	});*/
+	app.get('/', function(req, res){
+		console.log("router line 21 starting getWeather");
+		seneca.act({ role: 'weather', cmd: 'getWeather',city:'Brussels' }, function(err, result){
+			if (err) console.log('ERROR: ', err);
+			else {
+				console.log(result.html);
+				res.status(200).render(__dirname + '/templates/index.jade', {
+					weather: result.html
+				});
+			}
+		});
+
+
+
 	});
 }
